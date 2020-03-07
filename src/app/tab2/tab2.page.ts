8aab65ae37118acb4fdd64832a704587edbccb66
HBSIS <hbsis@proway.treina>
2020-03-07 17:27:36 -0300

    Page details

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,6 +3,9 @@ import { Observable } from 'rxjs';
 import { SolicitacoesDTO } from 'src/models/solicitacoes.dto';
 import { Component } from '@angular/core';
 import { NavController, ToastController } from '@ionic/angular';
+import { DetalhesPage } from '../detalhes/detalhes.page';
+import { Route, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-tab2',
@@ -20,26 +23,31 @@ export class Tab2Page {
   constructor(public navCtrl: NavController,
               public solicitacoesService: SolicitacoesService,
               private toastController: ToastController, 
+              private routes : Router
              ){
     this.getSolciitacoes();
   };
+
+  goAnOtherPage() {
+    this.navCtrl.navigateRoot('DetalhesPage')
+  }
 
   getSolciitacoes(){
     this.data = this.solicitacoesService.listar();
     this.data.subscribe(data =>{
       this.items = data;
       console.log(this.items)
-    })
+      
+    });
   }
 
   goToSolicitacao(id: string){
     this.datas = this.solicitacoesService.getSolicitacao(id);
-      this.datas.subscribe(async () =>{ 
-        this.solicitacao = await this.datas
-    
-  })
-    console.log(this.solicitacao)
-
+      this.datas.subscribe(datas=>{
+        this.solicitacao = datas;
+        console.log(this.solicitacao)
+      });   
+      this.routes.navigate(["/detalhes"])
   }
 
 }
